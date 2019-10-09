8633bd5cb51f92880f43c4d3270a581a429d281c
Andrey4901 <andrey.willians@gmail.com>
2019-10-09 11:28:54 -0300

    aula 09/10

--- main.c
+++ main.c
@@ -12,17 +12,23 @@
 */
 #include "tetris.h"
 #include "display.h"
+#define DEBUG 0 // ← alterar se precisar debugar
+
 
 //          Função principal
 
 int main(){
     char matrix[ROWS][COLUMNS];
-    int posI, posJ;
+    Bloco tijolo;
     int keypressed=0;
     
     //posicao inicial do personagem
-    posI = 0;
-    posJ = COLUMNS/2;
+    tijolo.i = 0;
+    tijolo.j = COLUMNS/2;
+    tijolo.tipo = TIPO_I;
+    tijolo.orientacao = ORIENTACAO_UP;
+    tijolo.width = 1;
+    tijolo.height = 4;
     //inicializando matriz
     init(matrix);
 
@@ -34,14 +40,32 @@ int main(){
     while(keypressed != ESC){
         gotoxy(0,0);
 
-        matrix[posI][posJ] = '\xFE';
-
+        #if DEBUG == 1
+            printf("\xFE = (%d, %d)\n", tijolo.i, tijolo.j);
+        #endif
+        //desenha o pixel na tela
+        switch(tijolo.orientacao){
+            case ORIENTACAO_UP:
+                if(tijolo.i-3>=0) matrix[tijolo.i-3][tijolo.j] = PIXEL;
+                if(tijolo.i-2>=0) matrix[tijolo.i-2][tijolo.j] = PIXEL;
+                if(tijolo.i-1>=0) matrix[tijolo.i-1][tijolo.j] = PIXEL;
+                matrix[tijolo.i][tijolo.j] = PIXEL;
+                break;
+            case ORIENTACAO_LEFT:
+            //
+            break;
+        }
+        
         printMatrix(matrix);
         bordas(matrix);
 
-        matrix[posI][posJ] = ' ';
+        //apaga a posição anterior do pixel
+        if(tijolo.i-3>=0) matrix[tijolo.i-3][tijolo.j] = EMPTY;
+        if(tijolo.i-2>=0) matrix[tijolo.i-2][tijolo.j] = EMPTY;
+        if(tijolo.i-1>=0) matrix[tijolo.i-1][tijolo.j] = EMPTY;
+        matrix[tijolo.i][tijolo.j] = EMPTY;
         
-        if(posI < ROWS-2) posI++;
+        if(tijolo.i < ROWS-2) tijolo.i++;
 
         //lê as teclas
         keypressed = 0;
@@ -50,14 +74,14 @@ int main(){
         
 
         switch (keypressed){
-        	case 65:
-            case TECLA_A:
+        	case tecla_A:
+            case TECLA_a:
             case LEFT: 
-                if(posJ > 0) posJ--; break;
-            case 68:
-            case TECLA_D:
+                if(tijolo.j > 0) tijolo.j--; break;
+            case tecla_D:
+            case TECLA_d:
             case RIGHT:
-                if(posJ < (COLUMNS-1)) posJ++; break;
+                if(tijolo.j < (COLUMNS-1)) tijolo.j++; break;
             
         }
 
--- tetris.h
+++ tetris.h
@@ -19,8 +19,37 @@
 #define ARROWS 224
 #define LEFT 75
 #define RIGHT 77
-#define TECLA_A 97
-#define TECLA_D 100
+#define TECLA_a 97
+#define TECLA_d 100
+#define tecla_A 65
+#define tecla_D 68
+//Orientações p/ as peças
+#define ORIENTACAO_UP 1
+#define ORIENTACAO_DOWN 2
+#define ORIENTACAO_LEFT 3
+#define ORIENTACAO_RIGHT 4
+//Tipos de peças
+#define TIPO_L 1
+#define TIPO_L_R 2 //L reverso
+#define TIPO_T 3
+#define TIPO_Z 4
+#define TIPO_Z_R 5 //Z reverso
+#define TIPO_O 6   //Quadrado
+#define TIPO_I 7
+// configurar layout
+#define PIXEL 254 
+#define EMPTY 32 
+
+// Estrutura padrão de componentes
+typedef struct {
+    int i; //posic. nas linhas
+    int j; //posic. nas colunas
+    int orientacao; 
+    int tipo; //(7 peças)
+    int width; //largura peça
+    int height; //altura peça
+}Bloco;
+
 
 //Assinaturas
 
